c67ae57daad888190905227194fee69860f510a0
dmitriy levykin <dmlegasy@gmail.com>
2026-09-13 13:27:40 +0300

    feat(core): add setup bot

---
+++ bot/core/setup.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.base import DefaultKeyBuilder
+from aiogram.fsm.storage.redis import RedisStorage
+
+from bot.core.config import settings
+from bot.core.cache import get_redis
+
+
+def create_bot() -> Bot:
+    return Bot(
+        token=settings.BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+
+
+def create_dispatcher() -> Dispatcher:
+    key_builder = DefaultKeyBuilder(with_destiny=True)
+    storage = RedisStorage(get_redis(), key_builder=key_builder)
+    return Dispatcher(storage=storage)
